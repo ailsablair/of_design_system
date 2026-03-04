d22fc9e759d1fdafa477c369d4de0b7bb0f0e567
Ailsa Blair <109297718+ailsablair@users.noreply.github.com>
2026-03-04 09:26:43 -0800

    update

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -7,27 +7,7 @@ module.exports = async ({ config }) => {
       {
         loader: 'file-loader',
         options: {
-	name: 'Aileron-Black-webfont.woff', 
-	name: 'Aileron-BlackItalic-webfont.woff', 
-	name: 'Aileron-Bold-webfont.woff', 
-	name: 'Aileron-BoldItalic-webfont.woff', 
-	name: 'Aileron-Heavy-webfont.woff', 
-	name: 'Aileron-HeavyItalic-webfont.woff', 
-	name: 'Aileron-Italic-webfont.woff', 
-	name: 'Aileron-Light-webfont.woff', 
-	name: 'Aileron-LightItalic-webfont.woff', 
-	name: 'Aileron-Regular-webfont.woff', 
-	name: 'Aileron-SemiBold-webfont.woff', 
-	name: 'Aileron-SemiBoldItalic-webfont.woff', 
-	name: 'Aileron-Thin-webfont.woff', 
-	name: 'Aileron-ThinItalic-webfont.woff', 
-	name: 'Aileron-UltraLight-webfont.woff', 
-	name: 'Aileron-UltraLightItalic-webfont.woff', 
-	name: ‘BarnebokBlack.woff', 
-	name: ‘BarnebokBold.woff', 
-	name: ‘BarnebokLight.woff', 
-	name: ‘BarnebokMedium.woff', 
-	name: ‘BarnebokRegular.woff', 
+      	name: 'src/assets/fonts/[name][ext]',
         },
       },
     ],
@@ -35,4 +15,4 @@ module.exports = async ({ config }) => {
   });
 
   return config;
-};   
+};
