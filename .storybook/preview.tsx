5ed7b7cfaa26aaadb5ade2ecfddb2814b4f88f43
Ailsa Blair <109297718+ailsablair@users.noreply.github.com>
2026-03-04 09:56:25 -0800

    custom fonts

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,5 +1,5 @@
 import type { Preview } from "@storybook/react";
-
+import '../src/index.css';   
 import { Global, css, ThemeProvider } from '@emotion/react';
 import { withThemeFromJSXProvider } from '@storybook/addon-themes';
 
